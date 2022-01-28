6c53a15c81faeebbab6a1f95f2bd433a4391a835
Jonathansr16 <Jonathansr2025@gmail.com>
2022-01-28 09:17:09 -0600

    Formularios agregados

---
+++ src/app/pages/pagaqui/pagaqui.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagaqui',
+  templateUrl: './pagaqui.component.html',
+  styleUrls: ['./pagaqui.component.css']
+})
+export class PagaquiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/pages/recargaki/recargaki.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recargaki',
+  templateUrl: './recargaki.component.html',
+  styleUrls: ['./recargaki.component.css']
+})
+export class RecargakiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
